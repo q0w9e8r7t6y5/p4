2c2e9fc773ab2deb170e4d0ecb1dfeb54248e8bc
q0w9e8r7t6y5 <syeety@me.com>
2018-12-18 17:50:54 -0500

    fixed pivot

--- app/Http/Controllers/OrderListController.php
+++ app/Http/Controllers/OrderListController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
+use App\Order;
+use App\OrderList;
 use Illuminate\Http\Request;
 
 class OrderListController extends Controller
 {
     public function index()
     {
+
+        $users = Order::orderBy('id')->get();
+
+        dump($users->ToArray());
+
         return view('orderlist');
     }
 
