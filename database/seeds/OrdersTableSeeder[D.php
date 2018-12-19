dc3717db898ea4ac7cbcd941848948b943a9e82f
q0w9e8r7t6y5 <syeety@me.com>
2018-12-19 18:12:15 -0500

    second seed done

--- database/seeds/OrdersTableSeeder[D.php
+++ database/seeds/OrdersTableSeeder[D.php
@@ -14,9 +14,44 @@ class OrdersTableSeeder extends Seeder
     {
         $order = Order::updateOrCreate(
             ['fill' => 'Chicken', 'rice' => 'White Rice', 'type' => 'Burrito',
-                "beans" => 'Yes', 'veggies' => 'Yes', 'toppings' => 'None',
+                "beans" => 'Yes', 'veggies' => 'No', 'toppings' => 'None',
                 'special_note' => 'tortilla on the side please', 'price' => '$7.95',
-                'order_number' => 'SY1111', "customer_id" => '1']
+                'order_number' => 'SY11111', "customer_id" => '1']
+        );
+
+        $order = Order::updateOrCreate(
+            ['fill' => 'Carnitas', 'rice' => 'Brown Rice', 'type' => 'Burrito Bowl',
+                "beans" => 'No', 'veggies' => 'Yes', 'toppings' => 'Queso, Guac',
+                'special_note' => 'extra meat', 'price' => '$7.95',
+                'order_number' => 'SY22222', "customer_id" => '2']
+        );
+
+        $order = Order::updateOrCreate(
+            ['fill' => 'Steak', 'rice' => 'None', 'type' => 'Hard Shell Taco',
+                "beans" => 'Yes', 'veggies' => 'No', 'toppings' => 'Hot Salsa, Cheese',
+                'special_note' => 'double shells please', 'price' => '$7.95',
+                'order_number' => 'SY33333', "customer_id" => '3']
+        );
+
+        $order = Order::updateOrCreate(
+            ['fill' => 'Chorizo', 'rice' => 'White Rice', 'type' => 'Soft Shell Taco',
+                "beans" => 'No', 'veggies' => 'Yes', 'toppings' => 'Mild Salsa, Queso',
+                'special_note' => 'None', 'price' => '$7.95',
+                'order_number' => 'SY44444', "customer_id" => '4']
+        );
+
+        $order = Order::updateOrCreate(
+            ['fill' => 'Barbacoa', 'rice' => 'Brown Rice', 'type' => 'Salad',
+                "beans" => 'Yes', 'veggies' => 'No', 'toppings' => 'Hot Salsa, Cheese, Guac, Sour Cream',
+                'special_note' => 'extra lettuce', 'price' => '$7.95',
+                'order_number' => 'SY55555', "customer_id" => '5']
+        );
+
+        $order = Order::updateOrCreate(
+            ['fill' => 'Sofritas', 'rice' => 'None', 'type' => 'Burrito',
+                "beans" => 'No', 'veggies' => 'Yes', 'toppings' => 'Med Corn Salsa',
+                'special_note' => 'to go on the side', 'price' => '$6.50',
+                'order_number' => 'SY66666', "customer_id" => '1']
         );
     }
 }
