14a7ff6513c66ef88bcfda8eea9d910b8a703039
q0w9e8r7t6y5 <syeety@me.com>
2018-12-19 23:17:48 -0500

    got rid of some comments

--- routes/web.php
+++ routes/web.php
@@ -38,36 +38,34 @@ Route::put('/edit/{id}', 'EditController@update');
 # DELETE
 # Show the page to confirm deletion of a user
 Route::get('/edit/{id}/delete', 'EditController@delete');
-//
 
-# NOT WORKING
 # Process the deletion of a user
 Route::delete('/edit/{id}', 'EditController@destroy');
 
-
-Route::get('/debug', function () {
-
-    $debug = [
-        'Environment' => App::environment(),
-        'Database defaultStringLength' => Illuminate\Database\Schema\Builder::$defaultStringLength,
-    ];
-
-    /*
-    The following commented out line will print your MySQL credentials.
-    Uncomment this line only if you're facing difficulties connecting to the
-    database and you need to confirm your credentials. When you're done
-    debugging, comment it back out so you don't accidentally leave it
-    running on your production server, making your credentials public.
-    */
-    #$debug['MySQL connection config'] = config('database.connections.mysql');
-
-    try {
-        $databases = DB::select('SHOW DATABASES;');
-        $debug['Database connection test'] = 'PASSED';
-        $debug['Databases'] = array_column($databases, 'Database');
-    } catch (Exception $e) {
-        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
-    }
-
-    dump($debug);
-});
+//
+//Route::get('/debug', function () {
+//
+//    $debug = [
+//        'Environment' => App::environment(),
+//        'Database defaultStringLength' => Illuminate\Database\Schema\Builder::$defaultStringLength,
+//    ];
+//
+//    /*
+//    The following commented out line will print your MySQL credentials.
+//    Uncomment this line only if you're facing difficulties connecting to the
+//    database and you need to confirm your credentials. When you're done
+//    debugging, comment it back out so you don't accidentally leave it
+//    running on your production server, making your credentials public.
+//    */
+//    #$debug['MySQL connection config'] = config('database.connections.mysql');
+//
+//    try {
+//        $databases = DB::select('SHOW DATABASES;');
+//        $debug['Database connection test'] = 'PASSED';
+//        $debug['Databases'] = array_column($databases, 'Database');
+//    } catch (Exception $e) {
+//        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+//    }
+//
+//    dump($debug);
+//});
