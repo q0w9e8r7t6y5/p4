1c1ee76756264df856d007d29c97f04bee65adc0
q0w9e8r7t6y5 <syeety@me.com>
2018-12-19 22:56:24 -0500

    w3

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,8 +6,6 @@
 
 @section('content')
 
-@include('modules.styling')
-
     <section id='allUsers'>
         <h4>User List</h4>
         <h6><b>Please click on a name to see more information or modify/delete the user from our database</b></h6>
